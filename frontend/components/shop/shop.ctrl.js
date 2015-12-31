0dd686eeaafc4795b17b9df033e1b12890b7c3e6
Venmteam <maxtor2808@gmail.com>
2015-12-31 12:04:53 +0200

    view of products

--- frontend/components/shop/shop.ctrl.js
+++ frontend/components/shop/shop.ctrl.js
@@ -1,3 +1,3 @@
-angular.module('app').controller('ShopCtrl', function($scope) {
-
+angular.module('app').controller('ShopCtrl', function($scope, Restangular, $stateParams) {
+  $scope.products = Restangular.all('products').getList({ page: $stateParams.page, per: 6 }).$object;
 });
